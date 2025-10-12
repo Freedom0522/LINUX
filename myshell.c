0ccc84a4cb4a67883fa7a2f52b8c758e6660fdfc
clz <13303657947@163.com>
2025-10-12 20:50:58 +0800

    shell

---
+++ myshell.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<assert.h>
+
+#define NUM 1024
+#define OPT_NUM 64
+
+char lineCommand[NUM];
+char *myargv[OPT_NUM];
+int lastCode = 0;
+int lastSig = 0;
+
+int main(){
+    while(1)
+    {
+        printf("用户名@主机名 当前路径# ");
+        fflush(stdout);
+
+        char *s = fgets(lineCommand,sizeof(lineCommand)-1,stdin);
+        assert(s!=NULL);
+        (void) s;
+
+        lineCommand[strlen(lineCommand)-1] = 0;
+
+        myargv[0] = strtok(lineCommand," ");
+        int i = 0;
+        if(myargv[0] != NULL && strcmp(myargv[0],"ls") == 0){
+            myargv[i++] = (char*)"--color=auto";
+        }
+
+        while(myargv[i++] = strtok(NULL," "));
+
+        if (myargv[1] != NULL&& strcmp(myargv[0],"cd")==0)
+        {
+            if(myargv[1] != NULL)chdir(myargv[1]);
+            continue;
+        }
+
+        if(myargv[0] != NULL && myargv[1] != NULL && strcmp(myargv[0],"echo")==0)
+        {
+            if(strcmp(myargv[1],"$") == 0)
+            {
+                printf("%d,%d\n",lastCode,lastSig);
+            }
+            else
+            {
+                printf("%s\n",myargv[1]);
+            }
+            continue;
+        }
+#ifdef DEBUG
+        for(int i = 0;myargv[i];i++)
+        {
+            printf("myargv[%d]:%s\n",i,myargv[i]);
+        }
+#endif
+        pid_t id = fork();
+        assert(id != -1);
+
+        if(id == 0)
+        {
+            execvp(myargv[0],myargv);
+            exit(1);
+        }
+
+        int status = 0;
+        pid_t ret = waitpid(id,&status,0);
+        assert(ret>0);
+        (void)ret;
+        lastCode = ((status>>8)&0xFF);
+        lastSig = (status & 0x7F);
+    }
+
+}
